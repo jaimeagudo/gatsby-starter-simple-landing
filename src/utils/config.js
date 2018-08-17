b212845d0a726303edf284d36fba33a8ce540be0
jaimeagudo <jaimeagudo@users.no.reply.github.com>
2018-08-17 18:28:52 +0200

    rename to FT

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,18 +2,19 @@ const colors = require("../styles/colors");
 
 module.exports = {
   pathPrefix: "/",
-  appName: "SimpleLanding",
-  siteTitle: "SimpleLanding - a landing page GatsbyJs starter",
-  siteUrl: "https://gssl.greglobinski.com",
+  appName: "Functional Team",
+  siteTitle: "Functional Team - a landing page GatsbyJs starter",
+  siteUrl: "http://functionalteam.eu",
   siteImage: "preview.jpg",
   siteLanguage: "en",
-  siteDescription: "SimpleLanding is a dead simple landing page GatsbyJs starter.",
+  siteDescription:
+    "Functional Team is a software house focused on functional programming consulting. We mainly work with Clojure & React to deliver simple solutions to complex problems.",
   contactEmail: "hello@example.com",
-  ctaLinkUrl: "https://dev.greglobinski.com/gatsby-starter-simple-landing/",
+  ctaLinkUrl: "https://github.com/jaimeagudo/",
   ctaLinkLabel: "GatsbyJS starter",
   // manifest.json
-  manifestName: "SimpleLanding - Gatsby starter",
-  manifestShortName: "SimplLanding",
+  manifestName: "Functional Team - Gatsby starter",
+  manifestShortName: "FunctionalTeam",
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.bg,
   manifestThemeColor: colors.bg,
